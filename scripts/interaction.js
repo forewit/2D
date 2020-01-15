0ff40edaaea2ca928d305693ba42d3c29128d6f8
Anderson <MDAnderson@samaritan.org>
2020-01-14 20:03:44 -0500

    removed hotkeys library

--- scripts/interaction.js
+++ scripts/interaction.js
@@ -10,16 +10,74 @@
 
     'use strict';
 
+    var me = {
+        layer: undefined,
+        selected: [],
+        pointer: {},
+        downKeys: {},
+        start: function (new_layer) {
+            me.layer = new_layer;
+            me.selected = [];
+            window.addEventListener('touchstart', startHandler, { passive: false });
+            window.addEventListener('mousedown', startHandler);
+            window.addEventListener('keydown', keydownHandler, { passive: false });
+            window.addEventListener('keyup', keyupHandler);
+        },
+        stop: function () {
+            me.layer = undefined;
+            me.selected = [];
+            window.removeEventListener('touchstart', startHandler);
+            window.removeEventListener('mousedown', startHandler);
+            window.removeEventListener('keydown', keydownHandler);
+            window.removeEventListener('keyup', keyupHandler);
+        },
+        setLayer: function (new_layer) {
+            me.selected = [];
+            me.layer = new_layer;
+        }
+    };
+
     // PRIVATE VARIABLES
-    var layer = undefined;
-    var pointer = {};
-    var selected = []
-    
     var _tapDelay = 10; // delay before long touch
     var _start = 0;
     var _moving = false;
     var _selectbox = false;
     var _onItem = false;
+    var _keys = {
+        Shift: 16,
+        Control: 17,
+        Alt: 18,
+        Meta: 91,
+        Escape: 27,
+        A: 65,
+        R: 82,
+        S: 83,
+        F5: 116,
+    };
+
+    function keydownHandler(e) {
+        me.downKeys[e.keyCode] = true;
+
+        // Ctrl + A
+        if (e.keyCode == _keys.A && me.downKeys[_keys.Control]) {
+            console.log("Ctrl + A");
+        }
+
+        // Ctrl + S
+        else if (e.keyCode == _keys.S && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
+            console.log("Interupted page save");
+            e.preventDefault();
+        }
+
+        // F5 or Ctrl + R
+        else if (e.keyCode == _keys.F5 || 
+            (e.keyCode == _keys.R && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey))) {
+            console.log("Interupted page reload");
+            e.preventDefault();
+        }
+    }
+
+    function keyupHandler(e) { me.downKeys[e.keyCode] = false; }
 
     // PRIVATE FUNCTIONS
     function startHandler(e) {
@@ -31,12 +89,12 @@
         if (e.type === 'mousedown') {
             window.addEventListener('mousemove', moveHandler, { passive: false });
             window.addEventListener('mouseup', endHandler);
-            pointer = { x: e.clientX, y: e.clientY };
+            me.pointer = { x: e.clientX, y: e.clientY };
         } else {
             window.addEventListener('touchmove', moveHandler, { passive: false });
             window.addEventListener('touchend', endHandler);
             window.addEventListener('touchcancel', endHandler);
-            pointer = copyTouch(e.targetTouches[0]);
+            me.pointer = copyTouch(e.targetTouches[0]);
             e.preventDefault();
             e.stopPropagation();
         }
@@ -47,11 +105,11 @@
 
     function moveHandler(e) {
         if (e.type == 'mousemove') {
-            pointer = { x: e.clientX, y: e.clientY };
+            me.pointer = { x: e.clientX, y: e.clientY };
             if (_onItem) {
                 // move items
                 console.log("moving item");
-            } else if (_selectbox || (!_moving && (hotkeys.isPressed('control') || hotkeys.isPressed('shift')))) {
+            } else if (_selectbox || (!_moving && (me.downKeys[_keys.Shift] || me.downKeys[_keys.Control]))) {
                 _selectbox = true;
                 // draw selectbox
                 console.log("drawing selectbox");
@@ -60,7 +118,7 @@
                 console.log("panning");
             }
         } else {
-            pointer = copyTouch(e.targetTouches[0]);
+            me.pointer = copyTouch(e.targetTouches[0]);
             e.preventDefault();
             e.stopPropagation();
             /////////////////////////
@@ -78,14 +136,14 @@
                 // add items in selectbox to selected
                 console.log("selecting items in selectbox");
             } else if (!_moving) {
-                if (!(hotkeys.isPressed('control') || hotkeys.isPressed('shift'))) {
-                    selected = [];
+                if (!(me.downKeys[_keys.Shift] || me.downKeys[_keys.Control])) {
+                    me.selected = [];
                     console.log("clearing selected");
                 }
                 // add item to selected
                 console.log("checking to add item to selected");
             }
-        } else if (e.targetTouches.length == 0 || e.targetTouches[0].identifier != pointer.identifier) {
+        } else if (e.targetTouches.length == 0 || e.targetTouches[0].identifier != me.pointer.identifier) {
             window.removeEventListener('touchmove', moveHandler);
             window.removeEventListener('touchend', endHandler);
             window.removeEventListener('touchcancel', endHandler);
@@ -103,27 +161,5 @@
         };
     }
 
-    var Interactions = {
-        start: function(new_layer) {
-            layer = new_layer;
-            selected = [];
-            window.addEventListener('touchstart', startHandler, { passive: false });
-            window.addEventListener('mousedown', startHandler);
-
-            // Interupt reload
-            hotkeys('f5,ctrl+r,cmd+r', function (event, handler) {
-                event.preventDefault()
-                alert('you tried to reload!')
-            });
-        },
-        stop: function() {
-            layer = undefined;
-            selected = [];
-            window.removeEventListener('touchstart', startHandler);
-            window.removeEventListener('mousedown', startHandler);
-
-            hotkeys.unbind('f5,ctrl+r,cmd+r');
-        }
-    };
-    return Interactions;
+    return me;
 }));
