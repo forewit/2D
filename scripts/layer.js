b0fd2510efc0a028494fd21f71725a3c470fe9e2
Marc Anderson <marcda495@gmail.com>
2020-01-30 00:28:33 -0500

    na

--- scripts/layer.js
+++ scripts/layer.js
@@ -29,35 +29,6 @@ class Layer {
         delete this.sprites[sprite.ID];
     }
 
-    intersections(coords, coords2) {
-        var intersections = [];
-
-        for (const i in this.sprites) {
-            var sprite = this.sprites[i];
-            var pos = sprite.position;
-            var size = new Point(
-                sprite.size.x * sprite.scale.x,
-                sprite.size.y * sprite.scale.y
-            );
-
-            if (coords2) {
-                var center = new Point(pos.x - size.x, pos.y - pos.y);
-                if (((center.x > coords.x && center.x < coords2.x) ||
-                    (center.x < coords.x && center.x > coords2.x)) &&
-                    ((center.y > coords.y && center.y < coords2.y) ||
-                        (center.y < coords.y && center.y > coords2.y))) {
-                    intersections.push(sprite);
-                    console.log("sprite in intersection box!");
-                }
-            } else if (coords.x > pos.x && coords.x < pos.x + size.x &&
-                coords.y > pos.y && coords.y < pos.y + size.y) {
-                intersections.push(sprite);
-            }
-        }
-
-        return intersections;
-    }
-
     render() {
         for (const i in this.sprites) {
             this.sprites[i].render();
